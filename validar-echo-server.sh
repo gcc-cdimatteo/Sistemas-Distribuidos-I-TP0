a39bd524021138c174c2976322826a9d6e570b1f
Carolina Di Matteo <cdimatteo@fi.uba.ar>
2024-09-02 17:20:02 -0300

    add timeout

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,9 +1,6 @@
 MSG='Testing Message From SH'
 
-NETWORK_INSPECT=$(docker network inspect tp0_testing_net)
-SERVER_IP=$(echo "$NETWORK_INSPECT" | awk -F'"' '/"Name": "server"/ {getline; getline; getline; print $4}' | cut -d'/' -f1)
-
-RESPONSE=$(echo $MSG | docker run --rm --platform linux/amd64 --network=tp0_testing_net -i subfuzion/netcat $SERVER_IP 12345)
+RESPONSE=$(echo $MSG | docker run --rm --platform linux/amd64 --network=tp0_testing_net -i subfuzion/netcat -w 5 server 12345)
 
 if [ "$RESPONSE" == "$MSG" ]; then echo "action: test_echo_server | result: success"
 else echo "action: test_echo_server | result: fail"
