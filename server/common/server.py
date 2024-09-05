a11abf3d793f696492e49eaab071b67b8faed1dc
Carolina Di Matteo <cdimatteo@fi.uba.ar>
2024-09-05 17:51:49 -0300

    trying to close server after client processing

--- server/common/server.py
+++ server/common/server.py
@@ -7,19 +7,25 @@ from common.client import Client
 from common.utils import Bet, store_bets, load_bets, has_won
 
 class Server:
+    MAX_CLIENTS = 5
+
     def __init__(self, port, listen_backlog):
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
-        self._server_running = True
 
         manager = Manager()
 
+        ## Server Management
+        self.server_space = manager.Namespace()
+        self.server_space.server_running = True
+        self.server_space_lock = manager.Lock()
+
         ## Clients Management
-        self.winners_barrier = manager.Barrier(5) ## ONLY WORKS FOR 5 AGENCIES - TODO: fix
-        self.clients = manager.list()
-        self.clients_lock = manager.Lock()
-        
+        self.winners_barrier = manager.Barrier(Server.MAX_CLIENTS) ## ONLY WORKS FOR 5 AGENCIES - TODO: fix
+        self.clients_connected = manager.list()
+        self.clients_connected_lock = manager.Lock()
+
         ## Bets Management
         self.bets = manager.list()
         self.bets_lock = manager.Lock()
@@ -28,14 +34,17 @@ class Server:
         signal.signal(signal.SIGTERM, self._handle_exit)
     
     def _handle_exit(self, signum, frame):
-        self._server_running = False
-        logging.debug(f"_handle_exit - try to aquire clients lock")
-        self.clients_lock.acquire()
-        logging.debug(f"aquire clients lock")
-        for client in self.clients:
+        self.server_space_lock.acquire()
+        self.server_space.server_running = False
+        self.server_space_lock.release()
+        logging.debug(f"_handle_exit - try to aquire clients connected lock")
+        self.clients_connected_lock.acquire()
+        logging.debug(f"_handle_exit - aquire clients connected lock")
+        for client in self.clients_connected:
             logging.warn(f'connection with address {client} gracefully closed')
             client.close()
-        self.clients_lock.release()
+        self.clients_connected_lock.release()
+        logging.debug(f"_handle_exit - release clients connected lock")
         self._server_socket.close()
         logging.warn(f'server gracefully exited')
 
@@ -47,22 +56,37 @@ class Server:
         communication with a client. After client with communucation
         finishes, servers starts to accept new connections again
         """
+        logging.info(f"action: start server | result: in progress")
+
         processes: list[Process] = []
-        
-        while self._server_running:
+
+        self.server_space_lock.acquire()
+        server_status = self.server_space.server_running
+        self.server_space_lock.release()
+        while server_status:
+            logging.info(f"action: start server | result: running")
             client_sock = self.__accept_new_connection()
             if client_sock != None:
                 processes.append(Process(target=self.__handle_client_connection, args=(client_sock,)))
                 processes[-1].start()
-        
+                self.server_space_lock.acquire()
+                server_status = self.server_space.server_running
+                self.server_space_lock.release()
+
+        logging.info(f"action: start server | result: stopped")
+
+        logging.info(f"action: process management | result: in progress")
+
         for p in processes:
             try:
-                p.close()
+                p.join()
             except:
                 logging.error(f"process didn't closed successfully: name = {p.name}, id = {p.pid}")
                 logging.critical(f"forcing process to shutdown: in progress")
                 logging.critical(f"SIGTERM sent to process returned [{p.terminate()}]")
                 logging.critical(f"forcing process to shutdown: finished")
+        
+        logging.info(f"action: process management | result: success")
 
     def __handle_client_connection(self, client_sock):
         """
@@ -72,23 +96,13 @@ class Server:
         client socket will also be closed
         """
         try:
+            logging.info(f"action: receive_message | status: in progress")
+
             client = self.start_client(client_sock)
             while not client.finished:
-                self.process_message(client)
-
-                logging.debug(f'__handle_client_connection - try to acquire clients lock - client: {client}')
-                self.clients_lock.acquire()
-                logging.debug(f'__handle_client_connection - acquire clients lock - client: {client}')
-                logging.debug(f'__handle_client_connection - self.clients:')
-                for c in self.clients: 
-                    if client.ip == c.ip: 
-                        logging.debug(f'__handle_client_connection - c is: {client}')
-                        client = c
-                    break
-
-                self.clients_lock.release()
-                logging.debug(f'__handle_client_connection - release clients lock - client: {client}')
-
+                client = self.process_message(client)
+            
+            logging.info(f"action: receive_message | status: finished | client: {client}")
         except OSError as e:
             logging.error(f"action: receive_message | result: fail | error: {e}")
 
@@ -110,66 +124,56 @@ class Server:
             return None
     
     def start_client(self, socket) -> Client:
+        logging.debug(f"start_client - try to aquire clients connected lock")
+        self.clients_connected_lock.acquire()
+        logging.debug(f"start_client - aquire clients connected lock")
+
+        self.clients_connected.append(socket)
+        logging.debug(f"start_client - append socket")
+
+        self.clients_connected_lock.release()
+        logging.debug(f"start_client - release clients connected lock")
+
         client = Client(socket)
         logging.debug(f"create new client {client}")
-        logging.debug(f"add_client - try to aquire clients lock")
-        self.clients_lock.acquire()
-        logging.debug(f"aquire clients lock")
-        self.clients.append(client)
-        logging.debug(f"append {client} to {self.clients}")
-        self.clients_lock.release()
-        logging.debug(f"release clients lock")
+
         return client
 
     def finish_client(self, client: Client):
-        logging.debug(f"finish_client - try to aquire clients lock")
-        self.clients_lock.acquire()
-        logging.debug(f"aquire clients lock")
-
         client.finish()
-        logging.debug(f"finish client {client}")
-
-        all_clients_finished = True
-
-        logging.debug(f"self.clients:")
-        for c in self.clients: logging.debug(f"{c}")
-        
-        for c in self.clients:
-            if not c.finished: 
-                logging.debug(f"client {c} didnt finish yet")
-                all_clients_finished = False
-                break
 
         client.close()
-        logging.debug(f"close client socket")
 
-        self.clients_lock.release()
-        logging.debug(f"release clients lock")
+        logging.debug(f"finish_client - try to aquire clients connected lock")
+        self.clients_connected_lock.acquire()
+        logging.debug(f"finish_client - aquire clients connected lock")
 
-        if all_clients_finished: 
-            logging.debug(f"shutdown server")
-            self._server_running = False
+        idx = 0
+        for c in self.clients_connected:
+            if c.getpeername()[0] == client.ip: 
+                self.clients_connected.pop(idx)
+                break
+            
+            idx += 1
+        logging.debug(f"finish_client - remove socket")
+
+        if len(self.clients_connected) == 0: 
+            self.server_space_lock.acquire()
+            self.server_space.server_running = False ## shutdown server on last client
+            self.server_space_lock.release()
+
+        logging.debug(f"finish_client - len(self.clients_connected) = {len(self.clients_connected)}")
+
+        self.clients_connected_lock.release()
+        logging.debug(f"finish_client - release clients connected lock")
     
     def process_message(self, client: Client):
         msg = client.recv()
 
-        logging.debug(f"MESSAGE RECEIVED from {client}: {msg.content}")
-
-        logging.debug(f'process_message - try to acquire clients lock - client: {client}')
-        self.clients_lock.acquire()
-        logging.debug(f'process_message - acquire clients lock - client: {client}')
-        logging.debug(f'process_message - self.clients:')
-        for c in self.clients: logging.debug(f'process_message - {c}')
-
-        self.clients_lock.release()
-        logging.debug(f'process_message - release clients lock - client: {client}')
-
         if msg.empty(): return
 
         if msg.is_BET(): 
-            (agency, rejected) = self.process_bets(msg)
-            self.change_client_id(client, agency)
-            logging.debug(f"msg.is_BET() - client is: {client}")
+            rejected = self.process_bets(msg)
             if rejected != 0: 
                 client.send(f"REJECTED {rejected}\n")
             else:
@@ -178,19 +182,22 @@ class Server:
         elif msg.is_END():
             client.send(f"END ACK\n")
             logging.debug('action: receive_end | result: success')
-            self.process_winners() ## blocks until last client, then load bets
+            self.process_winners() ## blocks until last client
         
         elif msg.is_CON():
-            logging.debug(f"client is: {client}")
             logging.debug('action: receive_ask_winners | result: in progress')
+            client.id = msg.get_agency_id()
+            logging.debug(f'client updated: {client}')
             self.return_winners(client)
             logging.debug('action: receive_ask_winners | result: success')
             self.finish_client(client)
         
         else:
             logging.error("action: receive_message | result: fail | error: message couldnt be parsed")
+        
+        return client
     
-    def process_bets(self, msg: Message) -> tuple[int, int]:
+    def process_bets(self, msg: Message) -> int:
         (bets, rejected_bets) = msg.get_bets()
 
         if (rejected_bets != 0):
@@ -203,69 +210,37 @@ class Server:
         store_bets(bets)
         self.bets_file_lock.release()
 
-        return (bets[0].agency, rejected_bets)
+        return rejected_bets
 
-    def change_client_id(self, client, agency):
-        if client.id != 0: return
-
-        logging.debug(f'change_client_id - try to acquire clients lock - client: {client}')
-        self.clients_lock.acquire()
-        logging.debug(f'change_client_id - acquire clients lock - client: {client}')
-
-        logging.debug(f'change_client_id - self.clients before change:')
-        for c in self.clients: logging.debug(f"{c}")
-        
-        mut_client = None
-        mut_pos = 0
-
-        for c in self.clients: 
-            if c.ip == client.ip: 
-                mut_client = c
-                break
-            mut_pos += 1
-        
-        mut_client.set_id(agency)
-
-        self.clients[mut_pos] = mut_client
-
-        logging.debug(f'change_client_id - self.clients after change:')
-        for c in self.clients: logging.debug(f"{c}")
-
-        self.clients_lock.release()
-        logging.debug(f'change_client_id - release clients lock - client: {mut_client}')
-    
     def process_winners(self):
-        logging.debug(f'---- wait for winner in progress')
-
-        if self.winners_barrier.n_waiting == 4: ## last ask
+        if self.winners_barrier.n_waiting == Server.MAX_CLIENTS - 1: ## last ask
             self.bets_lock.acquire() ## at this point nobody should have the lock
             self.bets[:] = list(load_bets())
-            self.load_winners()
             self.bets_lock.release()
             logging.info('action: sorteo | result: success')
-
-        logging.debug(f'---- wait for winner finished')
         
         self.winners_barrier.wait()
 
-    def load_winners(self):
-        winners = {}
-        for bet in self.bets:
-            if has_won(bet):
-                if bet.agency not in winners: winners[bet.agency] = []
-                winners[bet.agency].append(bet)
-        
-        logging.debug(f'try to acquire clients lock')
-        self.clients_lock.acquire()
-        logging.debug(f'acquire clients lock')
-        logging.debug(f'self.clients:')
-        for c in self.clients: logging.debug(f'{c}')
-
-        for c in self.clients:
-            c.set_winners(winners[c.id])
-
-        self.clients_lock.release()
-        logging.debug(f'release clients lock')
-    
     def return_winners(self, client: Client):
-        client.send(f"{'|'.join([winner.document for winner in client.winners])}\n")
+        winners = self.load_winners(client)
+        client.send(f"{'|'.join([winner.document for winner in winners])}\n")
+    
+    def load_winners(self, client: Client) -> list[Bet]:
+        logging.debug(f'load_winners - try to acquire winners lock - client {client}')
+        self.bets_lock.acquire()
+        logging.debug(f'load_winners - acquire winners lock - client {client}')
+
+        winners: list[Bet] = []
+
+        logging.debug(f'client {client} - self.bets has {len(self.bets)} rows')
+
+        for bet in self.bets:
+            if int(bet.agency) == int(client.id) and has_won(bet):
+                winners.append(bet)
+
+        logging.debug(f'winners for client {client} are {winners} rows')
+
+        self.bets_lock.release()
+        logging.debug(f'load_winners - release winners lock - client {client}')
+        
+        return winners
